3d92a2fccbe278643809d5db1a5e6c3e2ae56394
waterburner <Waterburner@users.noreply.github.com>
2023-01-10 19:39:26 -0600

    Homepage.js - creating future structure

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,8 +2,30 @@ import React from "react";
 
 export default function Homepage() {
     return (
-        <div>
-            <h2>Homepage</h2>
+        <div className="homepage-wrapper">
+            <div className="homepage">
+                <div className="title-wrapper">
+                    <div className="title">
+                        <h2>Homepage</h2>
+                    </div>
+                </div>
+                <div className="photo-section-wrapper">
+                    <div className="photo-section">
+                        <>
+                            big photo on the background with or without some
+                            text on top
+                        </>
+                    </div>
+                </div>
+                <div className="options-section-wrapper">
+                    <div className="options-section">
+                        <>little photo with hover effect</>
+                        <>little photo with hover effect</>
+                        <>little photo with hover effect</>
+                        <>little photo with hover effect</>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
