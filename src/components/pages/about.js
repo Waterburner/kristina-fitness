5ddd5fdb4de193f9e333a3ead74460ff282f383f
waterburner <Waterburner@users.noreply.github.com>
2023-03-23 01:31:58 -0500

    created AboutPage component

---
+++ src/components/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AboutPage() {
+    return (
+        <div className="about-wrapper">
+            <h3 className="about">About page</h3>
+        </div>
+    );
+}
