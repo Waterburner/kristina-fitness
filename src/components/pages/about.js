960c2b97a7c32faec7cf986496502a606b995101
waterburner <Waterburner@users.noreply.github.com>
2023-03-23 01:52:22 -0500

    added content base to about.js component

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -3,7 +3,47 @@ import React from "react";
 export default function AboutPage() {
     return (
         <div className="about-wrapper">
-            <h3 className="about">About page</h3>
+            <div className="about">
+                <div className="about-me-wrapper section-wrapper">
+                    <div className="about-me section">
+                        <div className="description-wrapper">
+                            <h3 className="description-header">
+                                About Kristina
+                            </h3>
+                            <p className="description">
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Molestias aut iusto iure
+                                veritatis pariatur expedita ex? Accusantium
+                                itaque repellat neque facere quia ducimus et?
+                                Quibusdam praesentium voluptatum animi fugiat
+                                voluptas nihil doloribus iste repellendus,
+                                eligendi non sapiente omnis harum, suscipit a
+                                nisi deleniti officia blanditiis est ad ipsam
+                                aliquam quam dicta minima pariatur? Adipisci
+                                dolores illo qui facilis quam quisquam velit,
+                                eos atque vel, assumenda, eius optio quis cum in
+                                iure beatae ea excepturi numquam deleniti
+                                accusantium? Autem enim quo sequi! Molestias
+                                possimus perferendis harum consequuntur sequi ut
+                                quasi magni nobis dolores quae. Nemo aut
+                                deleniti a magnam repellat quo deserunt
+                                blanditiis soluta possimus? Sunt, non ad libero
+                                aperiam esse aut quis repellat modi ipsam cum,
+                                magni culpa? Aliquid, maiores libero. Itaque
+                                nulla incidunt modi pariatur laborum quibusdam?
+                            </p>
+                        </div>
+                        <div className="description-photo-wrapper">
+                            <div className="description-photo">
+                                cool photo header
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="section-wrapper">
+                    <div className="what-we-do section">pictures etc, idk</div>
+                </div>
+            </div>
         </div>
     );
 }
