d3d67a33bef2bb36cf8f2d7138d59b88503e6b0a
waterburner <Waterburner@users.noreply.github.com>
2023-03-23 01:33:28 -0500

    created Schedule.js component

---
+++ src/components/pages/schedule.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SchedulePage() {
+    return (
+        <div className="Schedule-wrapper">
+            <h3 className="schedule">Schedule Page</h3>
+        </div>
+    );
+}
