331cdde39b4560041c328d7c0ee1ed436bfdd2ae
waterburner <Waterburner@users.noreply.github.com>
2023-03-23 01:56:19 -0500

    added base content to schedule.js

--- src/components/pages/schedule.js
+++ src/components/pages/schedule.js
@@ -3,7 +3,37 @@ import React from "react";
 export default function SchedulePage() {
     return (
         <div className="Schedule-wrapper">
-            <h3 className="schedule">Schedule Page</h3>
+            <div className="schedule">
+                <div className="section-wrapper">
+                    <div className="section">cool picture</div>
+                </div>
+                <div className="schedule-description-wrapper">
+                    <div className="schedule-description description">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Dignissimos quisquam repudiandae accusantium
+                        necessitatibus, dolore perspiciatis eum facere ea natus
+                        enim voluptatibus. Illum, vero deleniti exercitationem
+                        nobis dolorem, praesentium qui eveniet odio rem
+                        doloremque magni maxime consectetur ipsa. Saepe fugit
+                        possimus tenetur labore necessitatibus, libero, tempora
+                        provident illum maiores rerum alias iusto quibusdam
+                        aspernatur consequuntur distinctio. Ipsam eius, est
+                        quidem nisi magni sed, deserunt cumque ipsa sapiente
+                        libero debitis culpa provident possimus neque, labore
+                        atque necessitatibus voluptas dolore qui voluptates
+                        recusandae nam quasi? Obcaecati inventore perspiciatis
+                        cumque deserunt! Soluta cumque hic quisquam vitae, a,
+                        asperiores iste eveniet cum eos nam eaque cupiditate
+                        modi tempore tempora error ipsum nobis atque! Accusamus
+                        esse modi asperiores optio, a placeat maiores! Vitae
+                        beatae voluptates nobis dolorum ea! Nisi eligendi
+                        expedita hic voluptates vitae!
+                    </div>
+                </div>
+                <div className="schedule-photo-wrapper">
+                    <div className="schedule-photo">cool photo</div>
+                </div>
+            </div>
         </div>
     );
 }
