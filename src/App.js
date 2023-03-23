e9c09e0191504894c158e89717ffc85f6c95ee49
waterburner <Waterburner@users.noreply.github.com>
2023-03-23 01:21:10 -0500

    Creating pages pretty much

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Homepage from "./components/Homepage";
+import Navbar from "./components/navigation/navbar";
 
 export default class App extends Component {
     render() {
         return (
             <div className="app">
+                <Navbar />
+
                 <Router>
                     <div className="app-wrapper">
                         <Routes>
                             <Route exact path="/" element={<Homepage />} />
+                            {/* <Route path="*" elesemt={<NoMatch />} /> */}
                         </Routes>
                     </div>
                 </Router>
--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -1,5 +1,39 @@
 import React from "react";
 
+import { NavLink } from "react-router-dom";
+
 export default function Navbar() {
-    return <div className="navbar">navigation bar</div>;
+    return (
+        <div className="navbar-wrapper">
+            <div className="navbar">
+                <div className="left">
+                    <div className="burger-wrapper">
+                        <div className="burger">=</div>
+                    </div>
+                    <div className="navlist-wrapper">
+                        <div className="navlist">
+                            <ul>
+                                <NavLink to="/pages/schedule">
+                                    <li>Schedule / Розклад тренувань</li>
+                                </NavLink>
+
+                                <NavLink to="/pages/aboutme">
+                                    <li>About me / Про мене</li>
+                                </NavLink>
+                                <NavLink to="/pages/contacts">
+                                    <li>Contacts / контакти</li>
+                                </NavLink>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="center"></div>
+                <div className="right">
+                    <div className="logo-wrapper">
+                        <div className="logo">LOGO</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
