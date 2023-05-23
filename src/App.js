6c3ab53fc97d9993f82f74e31d6ac647b8f0e6ed
waterburner <Waterburner@users.noreply.github.com>
2023-05-23 18:36:37 -0500

    Connected About.js at Routes in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Homepage from "./components/Homepage";
 import Navbar from "./components/navigation/Navbar";
+import About from "./components/pages/About";
 import NoMatch from "./components/NoMatch";
 
 export default class App extends Component {
@@ -16,6 +17,7 @@ export default class App extends Component {
                     <div className="app-wrapper">
                         <Routes>
                             <Route exact path="/" element={<Homepage />} />
+                            <Route path="/pages/aboutme" element={<About />} />
                             <Route path="*" elesemt={<NoMatch />} />
                         </Routes>
                     </div>
