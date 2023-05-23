17480a304f057fdb0012c5889823a610b0963690
waterburner <Waterburner@users.noreply.github.com>
2023-05-23 18:33:38 -0500

    added NoMatch to Routes at App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Homepage from "./components/Homepage";
 import Navbar from "./components/navigation/Navbar";
+import NoMatch from "./components/NoMatch";
 
 export default class App extends Component {
     render() {
@@ -15,7 +16,7 @@ export default class App extends Component {
                     <div className="app-wrapper">
                         <Routes>
                             <Route exact path="/" element={<Homepage />} />
-                            {/* <Route path="*" elesemt={<NoMatch />} /> */}
+                            <Route path="*" elesemt={<NoMatch />} />
                         </Routes>
                     </div>
                 </Router>
