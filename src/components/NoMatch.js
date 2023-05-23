25243862fef08d223acede56b7e27012d029a39e
waterburner <Waterburner@users.noreply.github.com>
2023-05-23 18:52:51 -0500

    corrected language classes at NoMatch.js

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -4,15 +4,17 @@ export default function NoMatch() {
     return (
         <div className="no-match-wrapper">
             <div className="no-match">
-                <h2 className="no-match-title english">Page not found</h2>
-                <h2 className="no-match-title ukrainian">
+                <h2 className="no-match-title english_language">
+                    Page not found
+                </h2>
+                <h2 className="no-match-title ukrainian_language">
                     Сторінку не знайдено
                 </h2>
-                <p className="nomatch-description english">
+                <p className="nomatch-description english_language">
                     Make sure that url typed in correctly. Or we didn't make
                     this page yet
                 </p>
-                <p className="nomatch-description ukrainian">
+                <p className="nomatch-description ukrainian_language">
                     Перевірте правильність адреси. Можливо ми ще не створили цю
                     сторінку
                 </p>
