eb0ef8707d651dfa85afa90ec8e914cdc782daf6
waterburner <Waterburner@users.noreply.github.com>
2023-06-06 23:46:34 -0500

    Changed content at Contacts.js and added styles at contacts.scss

--- src/components/pages/Contacts.js
+++ src/components/pages/Contacts.js
@@ -12,7 +12,12 @@ export default function ContactPage() {
                         </div>
                         <div className="description-picture-wrapper">
                             <div className="description-picture">
-                                cool photo header
+                                <p className="phone-num">Phone: 123 123 1234</p>
+                                <p className="email">kristina@fitness.com</p>
+                                <p className="address">
+                                    Gym located at: 123 Street name, City,
+                                    Country
+                                </p>
                             </div>
                         </div>
                     </div>
