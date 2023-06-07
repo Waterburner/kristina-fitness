ca92f88034492f1da75df5a60bfe60afa1e1ee3e
waterburner <Waterburner@users.noreply.github.com>
2023-06-06 23:28:34 -0500

    changed content in About.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -11,26 +11,7 @@ export default function AboutPage() {
                                 About Kristina
                             </h3>
                             <p className="description">
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Molestias aut iusto iure
-                                veritatis pariatur expedita ex? Accusantium
-                                itaque repellat neque facere quia ducimus et?
-                                Quibusdam praesentium voluptatum animi fugiat
-                                voluptas nihil doloribus iste repellendus,
-                                eligendi non sapiente omnis harum, suscipit a
-                                nisi deleniti officia blanditiis est ad ipsam
-                                aliquam quam dicta minima pariatur? Adipisci
-                                dolores illo qui facilis quam quisquam velit,
-                                eos atque vel, assumenda, eius optio quis cum in
-                                iure beatae ea excepturi numquam deleniti
-                                accusantium? Autem enim quo sequi! Molestias
-                                possimus perferendis harum consequuntur sequi ut
-                                quasi magni nobis dolores quae. Nemo aut
-                                deleniti a magnam repellat quo deserunt
-                                blanditiis soluta possimus? Sunt, non ad libero
-                                aperiam esse aut quis repellat modi ipsam cum,
-                                magni culpa? Aliquid, maiores libero. Itaque
-                                nulla incidunt modi pariatur laborum quibusdam?
+                                Titles, education, experiences
                             </p>
                         </div>
                         <div className="description-photo-wrapper">
