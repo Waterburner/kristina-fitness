7762f8171459cae975173c2a3c6ebc0e0df52320
waterburner <Waterburner@users.noreply.github.com>
2023-07-18 23:06:44 -0500

    added ukrainian_language li items at about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -19,34 +19,66 @@ export default function AboutPage() {
                                 <ul className="description-list">
                                     <li className="description-list-item english_language">
                                         European champion at Kyokushin karate
+                                        (full contact)
+                                    </li>
+                                    <li className="description-list-item ukrainian_language">
+                                        Чемпіонка Європи з Кіокушин карате
+                                        (повний контакт)
                                     </li>
                                     <li className="description-list-item english_language">
                                         Silver medalist of World Championship at
                                         Kyokushin karate
                                     </li>
-                                    <li className="description-list-item english_language">
-                                        Multiple times Ukrainian champion at
-                                        Kyokushin karate
+                                    <li className="description-list-item ukrainian_language">
+                                        Срібна призерка Чемпіонату Світу з
+                                        Кіокушин карате
                                     </li>
                                     <li className="description-list-item english_language">
-                                        Multiple times Ukrainian champion at
+                                        Multiple times champion of Ukraine at
+                                        Kyokushin karate
+                                    </li>
+                                    <li className="description-list-item ukrainian_language">
+                                        Багато разова чемпіонка України з
+                                        Кіокушин карате
+                                    </li>
+                                    <li className="description-list-item english_language">
                                         Ukrainian Master of Sports (ukrainian
                                         title)
                                     </li>
+                                    <li className="description-list-item ukrainian_language">
+                                        Майстер спорту України з Кіокушин карате
+                                    </li>
+
                                     <li className="description-list-item english_language">
                                         Karate practitioner since 6 years old
+                                    </li>
+                                    <li className="description-list-item ukrainian_language">
+                                        Займається карате з 6 років
                                     </li>
                                     <li className="description-list-item english_language">
                                         Head and owner of the sport organization
                                         with about 300 students since 18 years
                                         old
                                     </li>
+                                    <li className="description-list-item ukrainian_language">
+                                        Голова спортивної організації з 18 років
+                                        в якій близько 300 учнів
+                                    </li>
                                     <li className="description-list-item english_language">
                                         Masters degree in sports, sports
                                         trainer, phisical reabilitation
                                     </li>
+                                    <li className="description-list-item ukrainian_language">
+                                        Магістр за спеціальністю тренер з виду
+                                        спорту, вчитель фізичної культури,
+                                        реабілітолог, викладач ВНЗ
+                                    </li>
                                     <li className="description-list-item english_language">
                                         Masters degree in Law (Ukraine)
+                                    </li>
+                                    <li className="description-list-item ukrainian_language">
+                                        Магістр юридичного факультету за
+                                        спеціальністю право
                                     </li>
                                 </ul>
                             </div>
