83a2029bfa32c4cfae8dc328e2d85cf9efb55515
waterburner <Waterburner@users.noreply.github.com>
2023-07-18 20:14:14 -0500

    added ukrainian_language class to Contacts.js

--- src/components/pages/Contacts.js
+++ src/components/pages/Contacts.js
@@ -7,16 +7,35 @@ export default function ContactPage() {
                 <div className="contacts-wrapper section-wrapper">
                     <div className="contacts section">
                         <div className="description-wrapper">
-                            <h3 className="description-header">Contacts</h3>
-                            <p className="description">How to get in touch</p>
+                            <h3 className="description-header english_language">
+                                Contacts
+                            </h3>
+                            <h3 className="description-header ukrainian_language">
+                                Контакти
+                            </h3>
+                            <p className="description english_language">
+                                How to get in touch
+                            </p>
+                            <p className="description ukrainian_language">
+                                Як зв'язатися
+                            </p>
                         </div>
                         <div className="description-picture-wrapper">
                             <div className="description-picture">
-                                <p className="phone-num">Phone: 123 123 1234</p>
+                                <p className="phone-num english_language">
+                                    Phone: 123 123 1234
+                                </p>
+                                <p className="phone-num ukrainian_language">
+                                    Телефон: 123 123 1234
+                                </p>
                                 <p className="email">kristina@fitness.com</p>
-                                <p className="address">
+                                <p className="address english_language">
                                     Gym located at: 123 Street name, City,
                                     Country
+                                </p>
+                                <p className="address ukrainian_language">
+                                    Спортивний зал розташований за адресою: 123
+                                    Street name, City, Country
                                 </p>
                             </div>
                         </div>
