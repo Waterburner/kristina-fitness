9d1181356202199fdcfb2706eaa57e9131ddac82
waterburner <Waterburner@users.noreply.github.com>
2023-07-18 22:32:42 -0500

    added className english_language to li at about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -17,34 +17,35 @@ export default function AboutPage() {
                         <div className="description-picture-wrapper">
                             <div className="description-picture">
                                 <ul className="description-list">
-                                    <li className="description-list-item">
+                                    <li className="description-list-item english_language">
                                         European champion at Kyokushin karate
                                     </li>
-                                    <li className="description-list-item">
+                                    <li className="description-list-item english_language">
                                         Silver medalist of World Championship at
                                         Kyokushin karate
                                     </li>
-                                    <li className="description-list-item">
+                                    <li className="description-list-item english_language">
                                         Multiple times Ukrainian champion at
                                         Kyokushin karate
                                     </li>
-                                    <li className="description-list-item">
+                                    <li className="description-list-item english_language">
+                                        Multiple times Ukrainian champion at
                                         Ukrainian Master of Sports (ukrainian
                                         title)
                                     </li>
-                                    <li className="description-list-item">
+                                    <li className="description-list-item english_language">
                                         Karate practitioner since 6 years old
                                     </li>
-                                    <li className="description-list-item">
+                                    <li className="description-list-item english_language">
                                         Head and owner of the sport organization
                                         with about 300 students since 18 years
                                         old
                                     </li>
-                                    <li className="description-list-item">
+                                    <li className="description-list-item english_language">
                                         Masters degree in sports, sports
                                         trainer, phisical reabilitation
                                     </li>
-                                    <li className="description-list-item">
+                                    <li className="description-list-item english_language">
                                         Masters degree in Law (Ukraine)
                                     </li>
                                 </ul>
